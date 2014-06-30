bf55d4458151e46c4539a50715d8aa74c0685f45
Evan Hahn <me@evanhahn.com>
2014-06-30 15:03:37 -0700

    first verison works (i think)

---
+++ index.js
@@ -0,0 +1,17 @@
+var MAX_SIZE = 2048;
+var ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+var alphabetLength = ALPHABET.length;
+
+function randomString() {
+  var length = Math.ceil(Math.random() * MAX_SIZE);
+  var result = '';
+  for (var i = 0; i < length; i ++) {
+    result += ALPHABET[(Math.random() * alphabetLength) | 0]; // x | 0 == Math.floor(x)
+  }
+  return result;
+}
+
+module.exports = function() {
+  return '<!--' + randomString() + '-->';
+};
