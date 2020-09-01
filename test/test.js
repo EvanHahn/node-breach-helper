eff3fb6ba752bac14e0e143f315316451093e353
Evan Hahn <me@evanhahn.com>
2020-09-01 09:10:18 -0500

    Update devDependencies and remove expect.js

--- test/test.js
+++ test/test.js
@@ -1,26 +1,23 @@
-var expect = require('expect.js');
-var helper = require('..');
+var assert = require("assert");
+var helper = require("..");
 
-describe('breach helper', function() {
-
+describe("breach helper", function () {
   var REPEATS = 1000;
 
-  it('returns an HTML comment with ASCII inside', function() {
-    for (var i = 0; i < REPEATS; i ++) {
-      expect(helper()).to.match(/^<!--\w+-->$/);
+  it("returns an HTML comment with ASCII inside", function () {
+    for (var i = 0; i < REPEATS; i++) {
+      assert(/^<!--\w+-->$/.test(helper()));
     }
   });
 
-  it('returns variable-length data', function() {
+  it("returns variable-length data", function () {
     var collisions = 0;
     var lastLength;
-    for (var i = 0; i < REPEATS; i ++) {
+    for (var i = 0; i < REPEATS; i++) {
       var length = helper().length;
-      if (length === lastLength)
-        collisions += 1;
+      if (length === lastLength) collisions += 1;
       lastLength = length;
     }
-    expect(collisions).to.be.lessThan(REPEATS / 100);
+    assert(collisions < REPEATS / 100);
   });
-
 });
